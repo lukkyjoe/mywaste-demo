8c8caf2d94088c3dc8e69f2668522a9b6b6f9230
Joseph Luk <joseph.sc.luk@gmail.com>
2021-11-14 18:58:45 -0800

    Remove app.use Stanford route

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const stanfordUniversityRouter = require('./routes/stanford-university')
+const stanfordUniversityRouter = require('./routes/stanford-university');
 
 var app = express();
 
@@ -24,15 +24,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/stanford-university', stanfordUniversityRouter)
+// app.use('/stanford-university', stanfordUniversityRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
